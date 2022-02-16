28db3aee08e8556b060a67ce28723df8aa3f5993
Lief Koepsel <lkoepsel@wellys.com>
2022-02-16 08:57:53 -0800

    added svg files as icons

--- Webserver/Webserver.ino
+++ Webserver/Webserver.ino
@@ -74,6 +74,21 @@ void setup(){
     request->send(SPIFFS, "/style.css", "text/css");
   });
 
+  // Route to load lightbulb.svg file
+  server.on("/lightbulb.svg", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/lightbulb.svg", "image/svg+xml");
+  });
+
+  // Route to load switch-closed.svg file
+  server.on("/switch-closed.svg", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/switch-closed.svg", "image/svg+xml");
+  });
+
+  // Route to load switch-open.svg file
+  server.on("/switch-open.svg", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/switch-open.svg", "image/svg+xml");
+  });
+
   // Route to set GPIO to HIGH
   server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
     digitalWrite(ledPin, HIGH);    
